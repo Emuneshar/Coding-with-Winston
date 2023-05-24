7c2f8c53d743282bc23f106859cd44ae35b9cc62
Eshwar Muneshar <Emuneshar@gmail.com>
2023-05-24 15:54:58 -0400

    Added menu for the project

--- BankProject.py
+++ BankProject.py
@@ -1,2 +1,10 @@
 print("Welcome to the Bank of Coach Eshwar")
-print("If you don't have an account you can open one now by selecting 4 from the menu below")
+print("If you don't have an account you can open one now by selecting 1 from the menu below")
+print(
+    "Your options are as follows:\n"
+    
+    "1. Open an account\n",
+    "2. Destroy your computer\n",
+    "3. Steal from bank\n",
+    "4. Get captured by the FBI\n"
+)
