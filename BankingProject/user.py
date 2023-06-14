634324d5457d9c8fa0ba4fbb2cb22d3e3168eb1c
Eshwar Muneshar <Emuneshar@gmail.com>
2023-06-14 16:28:45 -0400

    Added getter and setter

--- BankingProject/user.py
+++ BankingProject/user.py
@@ -1,5 +1,7 @@
 class user:
 
+    # This is our constructor function
+    
     def __init__ (self, firstName, lastName, userName, password, accountNumber, accountBalance):
         self.firstName = firstName
         self.lastName = lastName
@@ -7,4 +9,16 @@ class user:
         self.password = password
         self.accountNumber = accountNumber
         self.accountBalance = accountBalance
-        
+
+    # Getter and setter functions
+    # Each one must take "self" as a parameter 
+
+    # Getter
+    def getFirstName(self):
+        return self.firstName
+    
+    # Setter
+    def setFirstName(self, newFirstName):
+        self.firstName = newFirstName
+
+    
