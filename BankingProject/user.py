14f085dc2471935da2fbb86b417153f78ff02a39
Eshwar Muneshar <Emuneshar@gmail.com>
2023-06-14 16:19:39 -0400

    finished the constructor for User class

--- BankingProject/user.py
+++ BankingProject/user.py
@@ -3,4 +3,8 @@ class user:
     def __init__ (self, firstName, lastName, userName, password, accountNumber, accountBalance):
         self.firstName = firstName
         self.lastName = lastName
+        self.userName = userName
+        self.password = password
+        self.accountNumber = accountNumber
+        self.accountBalance = accountBalance
         
